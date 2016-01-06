c297880f3cea738ceb9c1b6703dacfcc6fe1b759
JesusEduardo2028 <jesusduardo2028@gmail.com>
2016-01-06 18:31:24 -0500

    add artist query styles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def spotify_image_tag(item)
+    if item.images.any? 
+      image_tag item.images.first["url"] , class: "lib-img-show artist-image" 
+    else
+      image_tag "default-image.png", class: "lib-img-show artist-image" 
+    end
+  end
 end
