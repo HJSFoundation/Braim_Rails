934d1aca71399fd0bb681b0e8ce98c22e1170998
JesusEduardo2028 <jesusduardo2028@gmail.com>
2016-01-09 19:57:59 -0500

    add record info to song progile

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -24,8 +24,24 @@ $(document).ready(function(){
       data: []
     }
   }
+  function show_recording_dara(performance_data){
+    $(".interest span").html(performance_data.emotiv_interest);
+    $(".engagement span").html(performance_data.emotiv_engagement);
+    $(".focus span").html(performance_data.emotiv_focus);
+    $(".relaxation span").html(performance_data.emotiv_relax);
+    $(".inst-excitement span").html(performance_data.emotiv_instantaneousExcitement);
+    $(".long-excitement span").html(performance_data.emotiv_longExcitement);
+    $(".stress span").html(performance_data.emotiv_stress);
+  }
   function draw(){
     time = time+1000;
+    console.log(time);
+    $("#message_time").html(time/1000);
+    if(time>currentSongDuration){
+      console.log("fin");
+      $("#stop_record_performance").click();
+    }
+    show_recording_dara(performance_data);
     interest = chart.series[0].data[chart.series[0].data.length-1].y;
     engagement = chart.series[1].data[chart.series[1].data.length-1].y;
     focus = chart.series[2].data[chart.series[2].data.length-1].y; 
@@ -44,7 +60,7 @@ $(document).ready(function(){
       timestamp: time 
     }
     recording.data.push(performance_data);
-    console.log(recording);
+    //console.log(recording);
   }
   $("#record_performance").on("click",function(e){
     e.preventDefault();
@@ -52,17 +68,22 @@ $(document).ready(function(){
     time = 0;
     $(this).toggleClass('disabled');
     $("#stop_record_performance").toggleClass('disabled');
+    $("#record_message").toggle()
+    $(".record-data").toggle()
     chart =  $("#container").highcharts();
     currentSongID = $(this).data("song-id");
+    currentSongDuration = $(this).data("song-duration");
     recording.song_id = currentSongID;
     timeStart = (new Date()).getTime();
-    console.log("start recording on: "+timeStart+" current song: "+currentSongID);
+    console.log("start recording on: "+timeStart+" current song: "+currentSongID+ "song duration: "+currentSongDuration);
     interval = setInterval(draw,1000);
   });
   $("#stop_record_performance").on("click",function(e){
     e.preventDefault();
     $(this).toggleClass('disabled');
     $("#record_performance").toggleClass('disabled');
+    $("#record_message").toggle()
+    $(".record-data").toggle()
     clearInterval(interval);
   });
 
