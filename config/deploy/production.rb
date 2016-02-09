791b4e9a8e19d340c6f557c9c236f51bfbf46391
JesusEduardo2028 <jesusduardo2028@gmail.com>
2016-02-09 15:42:03 -0500

    add production server ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,5 @@ set :branch , 'master'
 set :deploy_to, '/home/braim-pro/code/Braim_Rails'
 
 # Replace 127.0.0.1 with your server's IP address!
-server '40.78.19.2', user: 'braim-pro', roles: %w{web app}, my_property: :my_value
+server 'braim-pro.cloudapp.net', user: 'braim-pro', roles: %w{web app}, my_property: :my_value
 
